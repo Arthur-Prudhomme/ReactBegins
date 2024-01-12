cb07c6b17f2df0d26c2423b12ab51090d4cc871d
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2024-01-12 15:44:54 +0100

    display product by param

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,3 +1,34 @@
+import { useGetProductsQuery } from "../Services/API"
+import styled from 'styled-components';
+
 export default function () {
-    return <h1>Products</h1>
+    let { data, isFetching } = useGetProductsQuery();
+    return <div>
+        <h1>Products</h1>
+        {
+            isFetching ? <p>loading</p> : <div>
+                <br />
+                <ProductsList />
+            </div>
+        }
+    </div>
+}
+
+function ProductsList() {
+    let { data, isFetching } = useGetProductsQuery();
+    const queryParameters = new URLSearchParams(window.location.search);
+    const id = queryParameters.get("id")
+
+    return <div>
+        {
+            data.filter((products) => (
+                products.id === id
+            )).map(p => (
+                <div>
+                    <img src={p.image} /><br />
+                    <h3>{p.title}</h3>
+                </div>
+            ))
+        }
+    </div>
 }
